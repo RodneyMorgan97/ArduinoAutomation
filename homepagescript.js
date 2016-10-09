49d721fa4ae6ebbb938f72419e762ea873bc76a5
Rodney Morgan <Rodney Morgan>
2016-10-09 01:45:41 -0400

    added logic

--- homepagescript.js
+++ homepagescript.js
@@ -1,6 +1,6 @@
 function validateData()
 {
-	var field = document.getElementById("submit");
+	var field = document.getEl ementById("submit");
 	if (isNaN(field.value))
 	{
 		alert("You have entered information other than a number.");
